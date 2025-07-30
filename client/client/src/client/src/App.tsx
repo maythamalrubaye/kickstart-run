1662291842581bb3855862daeecead01c015a254
maythamalrubaye <maytham.alrubaye@gmail.com>
2025-07-30 21:33:22 +0200

    Create App.tsx

---
+++ client/client/src/client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>KickStart Run</h1>
+        <p>Youth Running Performance Platform</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
